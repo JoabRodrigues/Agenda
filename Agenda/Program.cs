d312ff473a340d0779e9cbb69bdba13f2c967da9
Joab Rodrigues <joabrodriguesfilho@gmail.com>
2019-09-03 21:00:12 -0300

    Add new projeto

---
+++ Agenda/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Agenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //teste
+        }
+    }
+}
